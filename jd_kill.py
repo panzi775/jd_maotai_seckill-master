6e03309861f2cbe5e94a544244f091faadc5516a
panzi775 <pangao775@qq.com>
2023-09-03 17:56:37 +0800

    feat:增加jd_kill文件,增加华为商城抢购

--- jd_kill.py
+++ jd_kill.py
@@ -1,5 +1,6 @@
 # 导入要使用到的模块(工具)
 import datetime
+import threading
 import time
 
 from selenium import webdriver
@@ -7,25 +8,32 @@ from selenium.webdriver.common.by import By
 import win32com.client
 from selenium.webdriver.support.wait import WebDriverWait
 
+import myPlatform
+
 speaker = win32com.client.Dispatch("SAPI.SpVoice")
 
 # 秒杀时间 算盘--软件 browser=浏览器
-times = '2023-08-27 18:33:00'
+times = '2023-09-02 18:00:00'
 # 打开浏览器
 browser = webdriver.Chrome()
 
+parm = myPlatform.PlatformParamsFactory.create_params(myPlatform.PlatformName.HUAWEI)
+
 # 进入京东网页
-browser.get("https://www.jd.com/")
+browser.get(parm.url)
 time.sleep(3)
 # 扫码登录
-browser.find_element(By.LINK_TEXT, "你好，请登录").click()
-print(f"请扫码")
 
-browser.find_element(By.LINK_TEXT, "扫码登录").click()
+
+browser.find_element(By.LINK_TEXT, parm.login).click()
+print("登录" + parm.login)
+
+if parm.loginWay is not None:
+    browser.find_element(By.LINK_TEXT, parm.loginWay).click()
 
 wait = WebDriverWait(browser, 300)
 
-target_string = "驿路惊鸿"
+target_string = parm.target
 
 found = False
 
@@ -33,15 +41,16 @@ while not found:
     try:
         # 查找页面上的元素\
         print("开始查找字符串")
-        element = browser.find_element(By.LINK_TEXT, target_string)
+        element = browser.find_element(By.PARTIAL_LINK_TEXT, target_string)
         print("目标字符串已找到：", target_string)
         found = True
         break  # 找到目标字符串后跳出循环
     except:
         pass  # 如果找不到元素，继续循环
 # 打开购物车页面
-browser.get("https://cart.jd.com/cart_index")
-time.sleep(5)
+if parm.platform is myPlatform.PlatformName.JD:
+    browser.get("https://cart.jd.com/cart_index")
+    time.sleep(5)
 '''
 1、打开浏览器
 2、登录京东账号
@@ -68,22 +77,25 @@ while True:
         # 点击结算按钮
         while 1 == 1:
             try:
-                if browser.find_element(By.LINK_TEXT, "去结算"):
+                if browser.find_element(By.LINK_TEXT, parm.order):
                     print("here")
-                    browser.find_element(By.LINK_TEXT, "去结算").click()
+                    browser.find_element(By.LINK_TEXT, parm.order).click()
                     print(f"主人,结算提交成功,我已帮你抢到商品啦,请及时支付订单")
-                    speaker.Speak(f"主人,结算提交成功,我已帮你抢到商品啦,请及时支付订单")
-                    speaker.Speak(f"赵银春，你是个大傻蛋")
+                    # speaker.Speak(f"主人,结算提交成功,我已帮你抢到商品啦,请及时支付订单")
                     break
             except:
                 pass
         # 点击提交订单按钮
         while True:
             try:
+                if browser.find_element(By.ID, parm.submit):
+                    browser.find_element(By.ID, parm.submit).click()
+                    print(f"抢购成功，请尽快付款")
+                    break
                 if browser.find_element(By.LINK_TEXT, "提交订单"):
                     browser.find_element(By.LINK_TEXT, "提交订单").click()
                     print(f"抢购成功，请尽快付款")
+                    break
             except:
                 print(f"主人,我已帮你抢到商品啦,您来支付吧")
-                break
-        time.sleep(0.01)
+                time.sleep(0.01)
---
+++ myPlatform.py
@@ -0,0 +1,36 @@
+from enum import Enum
+
+
+class PlatformName(Enum):
+    JD = 1
+    HUAWEI = 2
+    T_MALL = 3
+
+
+class PlatformParams:
+    def __init__(self, _platform, _url, _login, _order, _target, _submit=None, _login_way=None):
+        self.url = _url
+        self.login = _login
+        self.loginWay = _login_way
+        self.order = _order
+        self.submit = _submit
+        self.target = _target
+        self.platform = _platform
+
+
+class PlatformParamsFactory:
+    @staticmethod
+    def create_params(platform):
+        if platform == PlatformName.JD:
+            return PlatformParams(platform, "https://www.jd.com", "你好，请登录", _order="去结算",
+                                  _submit="order-submit", _target="驿路惊鸿",
+                                  _login_way="扫码登录", )
+        elif platform == PlatformName.HUAWEI:
+            return PlatformParams(platform, "https://www.vmall.com/product/10086327529191.html?cid=206781", "请登录",
+                                  "立即下单", _target="187",
+                                  _submit="checkoutSubmit")
+        elif platform == PlatformName.T_MALL:
+            return PlatformParams(platform, "https://www.vmall.com/product/10086009079805.html", "请登录", "立即下单",
+                                  "187")
+        else:
+            raise ValueError(f"Unsupported platform: {platform}")
